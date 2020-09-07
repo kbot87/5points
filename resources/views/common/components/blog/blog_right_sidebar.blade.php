68030c104e9a6bf0fb29a1d1fd9f9ee6261d5e38
Stas <green.kivi91@gmail.com>
2020-09-07 12:16:51 +0300

    add blades all pages

---
+++ resources/views/common/components/blog/blog_right_sidebar.blade.php
@@ -0,0 +1,9 @@
+<div class="blog_right_sidebar">
+    @include('common.components.sidebar.search_widget')
+    @include('common.components.sidebar.author_widget')
+    @include('common.components.sidebar.popular_post_widget')
+    @include('common.components.sidebar.ads_widget')
+    @include('common.components.sidebar.post_category_widget')
+    @include('common.components.sidebar.newsletter_widget')
+    @include('common.components.sidebar.tag_cloud_widget')
+</div>
---
+++ resources/views/common/components/blog_catalog.blade.php
@@ -0,0 +1,12 @@
+<section class="blog_area section-gap">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                @include('common.components.blog.blog_left_sidebar')
+            </div>
+            <div class="col-lg-4">
+                @include('common.components.blog.blog_right_sidebar')
+            </div>
+        </div>
+    </div>
+</section>
