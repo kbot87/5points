318a1909a7b48d312c24081e511ad305e0bd517f
kykyka <e.korj87@gmail.com>
2020-09-07 18:39:37 +0300

    admin panel 80%

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 
 
-        \App\Http\Middleware\localeMiddleware::class,
+        \App\Http\Middleware\LocaleMiddleware::class,
     ];
 
     /**
